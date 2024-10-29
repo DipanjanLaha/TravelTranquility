8cb3329e48d02908307605b1518fd7a0f269e7a2
Eshita <eshita1130.bh@gmail.com>
2024-10-29 09:21:02 +0530

    Login/Signup page replaced

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from '../components/LoginPage';
-import SignUp from '../components/SignPage';
+import Login from '../pages/LoginPage';
+import SignUp from '../pages/SignPage';
 
 const Home = () => {
   return (
