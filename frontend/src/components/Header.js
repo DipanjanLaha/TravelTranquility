5ac528614990e5ff0162b948424fff91c3996c95
Eshita <eshita1130.bh@gmail.com>
2024-11-27 00:06:10 +0530

    header changed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,16 +10,7 @@ const HeaderContainer = styled.header`
   background-color: transparent; /* Set background color to transparent */
 `;
 
-const FloatingContainer1 = styled.div`
-  position: fixed;
-  top: 45px;
-  height: 60px;
-  width: 550px;
-  left: 600px;
-  display: flex;
-  z-index: 1000;
-  border-radius: 50px;
-`;
+
 
 const FloatingContainer2 = styled.div`
   position: fixed;
@@ -29,34 +20,7 @@ const FloatingContainer2 = styled.div`
   z-index: 1000;
 `;
 
-const NavLinks = styled.ul`
-  list-style-type: none;
-  display: flex;
-  gap: 80px;
-  margin-top:2px;
-  padding: 15px;
 
-  li {
-    font-size: 18px;
-    font-weight: bold;
-    cursor: pointer;
-    transition: color 0.3s ease-in-out;
-
-    a {
-      text-decoration: none;
-      color: #444;
-      transition: color 0.3s ease-in-out;
-    }
-
-    a:hover {
-      color: #7aab35;
-    }
-
-    &.active a {
-      color: #7aab35;
-    }
-  }
-`;
 
 const LoginButton = styled(Link)`
   background-color: white;
@@ -81,16 +45,7 @@ const Header = () => {
   return (
     <>
       <HeaderContainer />
-      <FloatingContainer1>
-
-        <NavLinks>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/Destination">Destinations</Link></li>
-          <li><Link to="/Feedback">Feedbacks</Link></li>
-          <li><Link to="/Blog">Blog</Link></li>
-          <li><Link to="/Contact">Contact</Link></li>
-        </NavLinks>
-      </FloatingContainer1>
+      
       <FloatingContainer2>
         <LoginButton to="/login">Login / Signup</LoginButton>
       </FloatingContainer2>
